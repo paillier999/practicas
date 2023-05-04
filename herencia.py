470f380a6b4904a3b2046d9b1fa692ef67fd7498
Jaime <paillier999@hotmail.com>
2023-05-03 22:37:21 -0500

    agrego ejercicios de herencia multiple

--- herencia.py
+++ herencia.py
@@ -20,13 +20,50 @@ class Vehiculos:
               self.enmarca, "\nAcelerando: ", self.acelera, "\nFrenado: ", self.frena)
 
 
+class Furgoneta(Vehiculos):
+    def carga(self, cargar):
+        self.cargado = cargar
+        if self.cargado:
+            return "La furgoneta está cargada"
+        else:
+            return "La furgoneta no está cargada"
+
+
 class Moto(Vehiculos):
-    pass
+    hcaballito = ""
+    def caballito(self):
+        self.hcaballito = "Voy haciendo el caballito"
+
+    def estado(self):
+        print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn Marcha: ",
+              self.enmarca, "\nAcelerando: ", self.acelera, "\nFrenado: ", self.frena, "\n", self.hcaballito)
+
+
+class VElectricos():
+    def __init__(self):
+        self.autonomia=100
+
+    def cargarEnergia(self):
+
+        self.cargando = True
 
 
 miMoto = Moto("honda", "CRB")
 
-
-miMoto.frenar()
+miMoto.caballito()
 
 miMoto.estado()
+
+miFurgoneta=Furgoneta("Renault", "Kangoo")
+
+miFurgoneta.arrancar()
+
+miFurgoneta.estado()
+
+print(miFurgoneta.carga(True))
+
+class BicicletaEñlectrica(VElectricos, Vehiculos):
+
+    pass
+
+miBici= BicicletaElectrica()
